df5a3120ced3ec2738f93990e59d36153989d595
KSH UJAL SINGH <79851945+ujals@users.noreply.github.com>
2024-03-06 16:06:23 +0530

    Update flaskMapperHelium.py
    Removed the debugger mode

--- flaskMapperHelium.py
+++ flaskMapperHelium.py
@@ -220,4 +220,5 @@ def check_availability():
         return jsonify({"error": "Failed to fetch geocode data"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+
